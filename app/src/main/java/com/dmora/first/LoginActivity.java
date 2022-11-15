c260ee0db25ba265479cc790be169ec164336b2f
Daniel Mora <daniel27mora96@gmail.com>
2022-11-15 19:38:37 +0100

    Initial commit

---
+++ app/src/main/java/com/dmora/first/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.dmora.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView textSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textSignup = findViewById(R.id.textSignup);
+
+        textSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignup();
+            }
+        });
+
+        ImageView mDog = findViewById(R.id.dog);
+        ImageView mLogo = findViewById(R.id.logo);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+//                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(mDog);
+
+        Glide.with(this)
+                .load(R.drawable.avocado_del_diablo)
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mLogo);
+
+    }
+
+    public void openSignup(){
+        Intent intent = new Intent(this, SignupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+}
